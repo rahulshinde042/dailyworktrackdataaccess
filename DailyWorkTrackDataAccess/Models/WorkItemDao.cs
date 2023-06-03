afb9482f0ff93bbd31fbc9fcaf5f65d74d0f5dd6
Rahul Shinde <Rahul.Shinde1@wolterskluwer.com>
2023-06-03 13:48:50 +0530

    Folder structure created for Data Access project

---
+++ DailyWorkTrackDataAccess/Models/WorkItemDao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyWorkTrackDataAccess.Models
+{
+    /// <summary>
+    /// Model for the work item
+    /// </summary>
+    public class WorkItemDao
+    {
+        /// <summary>
+        /// Id of the work item
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Identifier for the user owning this tasks
+        /// </summary>
+        public int UserId { get; set; }
+        /// <summary>
+        /// Title for the work item eg. Discussion with team, Start working on story
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Date assigned for the work to be completed
+        /// </summary>
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Status of work item
+        /// </summary>
+        public int Status { get; set; }
+    }
+}
